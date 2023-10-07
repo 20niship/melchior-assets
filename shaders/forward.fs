02bbd821e3f806488f71ffde47d7a4d9e48a5627
20niship <idcnishimiya5378@gmail.com>
2023-10-08 05:40:02 +0900

    PBR lighting

--- shaders/forward.fs
+++ shaders/forward.fs
@@ -3,7 +3,6 @@
 uniform sampler2D texture_diffuse;
 uniform sampler2D texture_metallic;
 uniform sampler2D texture_roughness;
-
 struct Material {
   vec4 diffuse;
   float roughness;
@@ -13,6 +12,23 @@ struct Material {
 
 uniform Material material;
 uniform vec3 viewPos;
+struct Light {
+  vec3 Position;
+  vec4 Color;
+  float intensity;
+  float Linear;
+  float Quadratic;
+};
+
+const int NR_LIGHTS = 32;
+uniform Light lights[NR_LIGHTS];
+
+// IBL
+uniform samplerCube irradianceMap;
+uniform samplerCube prefilterMap;
+uniform sampler2D brdfLUT;
+
+const float PI = 3.14159265359;
 
 in vec2 Frag_uv;
 in vec3 FragPos;
@@ -31,6 +47,144 @@ in float cutting_plane_dist;
 
 out vec4 FragColor;
 
+// ----------------------------------------------------------------------------
+//
+vec3 getNormalFromMap(){
+    vec3 tangentNormal = Normal * 2.0 - 1.0;
+    vec3 Q1  = dFdx(FragPos);
+    vec3 Q2  = dFdy(FragPos);
+    vec2 st1 = dFdx(TexCoords);
+    vec2 st2 = dFdy(TexCoords);
+
+    vec3 N   = normalize(Normal);
+    vec3 T  = normalize(Q1*st2.t - Q2*st1.t);
+    vec3 B  = -normalize(cross(N, T));
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangentNormal);
+}
+
+float DistributionGGX(vec3 N, vec3 H, float roughness){
+    float a = roughness*roughness;
+    float a2 = a*a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float nom   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+// ----------------------------------------------------------------------------
+float GeometrySchlickGGX(float NdotV, float roughness){
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+// ----------------------------------------------------------------------------
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness){
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+vec3 fresnelSchlick(float cosTheta, vec3 F0){ return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);}
+vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness){ return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);}   
+
+
+vec3 calc_light(vec3 light_pos, vec3 light_color, float roughness, vec3 N, vec3 V, vec3 albedo, float metallic, vec3 F0, float attenuation){
+  vec3 L = normalize(light_pos - FragPos);
+  vec3 H = normalize(V + L);
+  float distance = length(light_pos - FragPos);
+  // float attenuation = 1.0 / (distance * distance);
+  vec3 radiance = light_color * attenuation;
+
+  // Cook-Torrance BRDF
+  float NDF = DistributionGGX(N, H, roughness);   
+  float G   = GeometrySmith(N, V, L, roughness);    
+  vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);        
+  
+  vec3 numerator    = NDF * G * F;
+  float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // + 0.0001 to prevent divide by zero
+  vec3 specular = numerator / denominator;
+  
+   // kS is equal to Fresnel
+  vec3 kS = F;
+  // for energy conservation, the diffuse and specular light can't
+  // be above 1.0 (unless the surface emits light); to preserve this
+  // relationship the diffuse component (kD) should equal 1.0 - kS.
+  vec3 kD = vec3(1.0) - kS;
+  // multiply kD by the inverse metalness such that only non-metals 
+  // have diffuse lighting, or a linear blend if partly metal (pure metals
+  // have no diffuse light).
+  kD *= 1.0 - metallic;	                
+      
+  // scale light by NdotL
+  float NdotL = max(dot(N, L), 0.0);
+
+  // add to outgoing radiance Lo
+  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+  return (kD * albedo / PI + specular) * radiance * NdotL; 
+}
+
+vec3 main_lighting(vec3 base_color, float roughness, float metallic, float ao){		
+  vec3 viewDir  = normalize(viewPos - FragPos);
+  vec3 albedo = pow(base_color, vec3(2.2));
+       
+  // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
+  // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)    
+  vec3 F0 = vec3(0.04); 
+  F0 = mix(F0, albedo, metallic);
+
+  // input lighting data
+  vec3 N = getNormalFromMap();
+  vec3 V = normalize(viewPos - FragPos);
+  vec3 R = reflect(-V, N); 
+
+  // reflectance equation
+  vec3 Lo = vec3(0.0);
+  for(int i = 0; i < NR_LIGHTS; ++i){
+    float distance = length(lights[i].Position - FragPos);
+   float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
+    //float attenuation = 1.0 / (distance * distance);
+    vec3 l = calc_light(lights[i].Position, lights[i].Color.rgb*lights[i].intensity , roughness, N, V, albedo, metallic, F0, attenuation);
+    Lo += max(l, 0);
+  }
+  return Lo;
+
+
+  // ambient lighting (we now use IBL as the ambient term)
+  vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
+  
+  vec3 kS = F;
+  vec3 kD = 1.0 - kS;
+  kD *= 1.0 - metallic;	  
+  
+  vec3 irradiance = texture(irradianceMap, N).rgb;
+  vec3 diffuse      = irradiance * albedo;
+  
+  // sample both the pre-filter map and the BRDF lut and combine them together as per the Split-Sum approximation to get the IBL specular part.
+  const float MAX_REFLECTION_LOD = 4.0;
+  vec3 prefilteredColor = textureLod(prefilterMap, R,  roughness * MAX_REFLECTION_LOD).rgb;    
+  vec2 brdf  = texture(brdfLUT, vec2(max(dot(N, V), 0.0), roughness)).rg;
+  vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
+
+  vec3 ambient = (kD * diffuse + specular) * ao;
+  // vec3 color = ambient + Lo;
+  vec3 color = Lo;
+  
+  color = color / (color + vec3(1.0));// HDR tonemapping
+  color = pow(color, vec3(1.0/2.2)); // gamma correct
+  return color;
+}
+
 void main(){
   uint type = material.type;
   uint lower= type & 0xF;  // 下位4ビットを取り出す
@@ -48,8 +202,10 @@ void main(){
   float roughness  = use_roughness_texture ? texture(texture_roughness, TexCoords).r : material.roughness;
   float metallic = use_metallic_texture ? texture(texture_metallic, TexCoords).r : material.metallic;
 
-  FragColor = vec4(diffuse, 1.0);
-  FragColor.r += metallic*0.001 + roughness*0.001;
+  // FragColor = vec4(diffuse, 1.0);
+  FragColor.rgb = main_lighting(diffuse, roughness, metallic, 1.0);
+  if(force_base_color) FragColor.rgb = diffuse;
+  FragColor.a = 1.0;
 
   if(use_vertex_color){
     FragColor.rgb = vertexColor.rgb;
