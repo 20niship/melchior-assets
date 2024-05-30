6aed57dbd261a53b569b6482a4ca5975f832b070
20niship <idcnishimiya5378@gmail.com>
2024-05-30 22:23:04 +0900

    update-models

---
+++ textures/resize.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import sys
+
+fname = sys.argv[1]
+image = Image.open(fname)
+resized_image = image.resize((512, 512))
+resized_image.save(fname)
+
+print("画像が正常にリサイズされました。")
+
